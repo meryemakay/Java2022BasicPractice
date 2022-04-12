d7587ad267159c915ea11fe6ca13fbf17767e366
Meryem Akay <haceryol491qgmail.com>
2022-04-12 23:59:08 +0300

    13.04.2022

---
+++ src/src/interviewQuestions4/Q03.java
@@ -0,0 +1,64 @@
+package src.interviewQuestions4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Q03 {
+    public static void main(String[] args) {
+
+// Kullanıcının yazdığı metni, 'hacker'ların konuşma diline çeviren bir method(method ismi hackerDili) yazınız.
+//    Hackerlar bazı harfleri sayılara çevirerek yazışabiliyorlar. Genellikle çevirdikleri harfler şu şekilde:
+//    s -> 5
+//    a -> 4
+//    e -> 3
+//    i -> 1
+//    o -> 0
+//    Test data
+//    hackerDili("java ile hersey guzel")
+//    j4v4 1l3 h3r53y guz3l
+//    İpucu harfleri değiştirin ve ekrana yazdırın.
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("bır cumle yazınız:");
+        String cumle = scan.nextLine().toLowerCase();
+        hackerDili(cumle);
+
+    }
+
+    private static void hackerDili(String cumle) {
+        String arr[] = new String[cumle.length()];
+
+        for (int i = 0; i < cumle.length(); i++) {
+            arr[i] = cumle.substring(i, i + 1);
+
+            System.out.print(arr[i]);
+
+        }
+        System.out.println();//bundan sonrakı satırda gelenler bırlemesın dıye bos sout attık
+
+        for (int i = 0; i < cumle.length(); i++) {
+            if (arr[i].contains("s")) {
+                arr[i] = "5";
+
+
+            }
+            if (arr[i].contains("a")) {
+                arr[i] = "4";
+
+            }
+            if (arr[i].contains("e")) {
+                arr[i] = "3";
+
+            }
+            if (arr[i].contains("i")) {
+                arr[i] = "1";
+
+            }
+
+        if (arr[i].contains("o")) {
+            arr[i] = "0";
+        }
+            System.out.print( arr[i]);
+    }
+
+}}
